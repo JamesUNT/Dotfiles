d0c0bf5f464a26a61535ba7c0e5a778ff4d96b42
JamesUNT <thiagoaugusto.inc@gmail.com>
2024-12-23 18:21:39 -0300

    configurando prettier e eslint

---
+++ nvim/nvim/lua/plugins/none-ls.lua
@@ -0,0 +1,25 @@
+return {
+  -- wraps linters and formaters as a vim LSP, making it easir to manage with Mason.
+  "nvimtools/none-ls.nvim",
+  dependencies = {
+    "nvimtools/none-ls-extras.nvim",
+  },
+  config = function()
+    local null_ls = require("null-ls")
+    null_ls.setup({
+      sources = {
+        null_ls.builtins.formatting.stylua,
+        null_ls.builtins.formatting.prettierd,
+        require("none-ls.diagnostics.eslint_d"),
+      },
+    })
+
+    -- applies format when saving any kind of file:
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      pattern = "*",
+      callback = function()
+        vim.lsp.buf.format()
+      end,
+    })
+  end,
+}
--- nvim/nvim/lua/plugins/treesitter.lua
+++ nvim/nvim/lua/plugins/treesitter.lua
@@ -1,13 +1,14 @@
-return 
-  {
-    "nvim-treesitter/nvim-treesitter", build = ":TSUpdate",
-    config = function()
-      local configs = require("nvim-treesitter.configs")
-      configs.setup({
-        ensure_installed = { "c", "lua", "vim", "vimdoc", "javascript", "html" },
-        sync_install = false,
-        highlight = { enable = true },
-        indent = { enable = true },  
-      })
-    end
-  }
+return {
+  -- highlighting code for better visualization.
+  "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
+  config = function()
+    local configs = require("nvim-treesitter.configs")
+    configs.setup({
+      ensure_installed = { "c", "lua", "vim", "vimdoc", "javascript", "typescript", "vue", "html" },
+      sync_install = false,
+      highlight = { enable = true },
+      indent = { enable = true },
+    })
+  end,
+}
