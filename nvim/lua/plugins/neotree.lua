75bfe2c3869062db4555cd725ec984bbf0f95c0f
JamesUNT <thiagoaugusto.inc@gmail.com>
2024-12-27 13:51:43 -0300

    configuring keymap for toggle neotree.

--- nvim/lua/plugins/neotree.lua
+++ nvim/lua/plugins/neotree.lua
@@ -1,15 +1,16 @@
 return {
-  {
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-tree/nvim-web-devicons",
-      "MunifTanjim/nui.nvim",
-    },
-    config = function()
-        -- NEOTree keymap configuration:
-      vim.keymap.set('n', '<leader>n', ':Neotree filesystem reveal left<CR>')
-    end
-  }
+	{
+		"nvim-neo-tree/neo-tree.nvim",
+		branch = "v3.x",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-tree/nvim-web-devicons",
+			"MunifTanjim/nui.nvim",
+		},
+		config = function()
+			-- NEOTree keymap configuration:
+			vim.keymap.set("n", "<leader>n", ":Neotree filesystem reveal left<CR>")
+			vim.keymap.set("n", "<leader>t", ":Neotree toggle<CR>")
+		end,
+	},
 }
